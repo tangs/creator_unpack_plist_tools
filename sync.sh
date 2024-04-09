2a993951f39d399dedcefb19774f11a304f8056f
tangs <tangsong1988kaka@gmail.com>
2024-04-09 12:22:01 +0800

    update.

--- sync.sh
+++ sync.sh
@@ -2,4 +2,4 @@
 
 # rm -rf /Volumes/tangsong/vegass/slots/
 # cp -rf /Users/tangs/Desktop/vegass/unzip/resources/slots/* /Volumes/tangsong/vegass/slots/
-rsync -ah --progress /Users/tangs/Desktop/vegass/unzip/resources/slots /Volumes/tangsong/vegass/slots
+rsync -ah --progress /Users/tangs/Desktop/vegass/unzip/resources/slots /Volumes/tangsong/vegass/
